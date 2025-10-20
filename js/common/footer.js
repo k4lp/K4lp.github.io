0a3d77e4fc8ef4e62379504c6be93ff1feb7de67
k4lp <61631873+k4lp@users.noreply.github.com>
2025-10-20 11:27:12 +0530

    Delete js directory

--- js/common/footer.js
+++
@@ -1,7 +0,0 @@
-export function initFooter() {
-    const container = document.getElementById('footer-container');
-    if (container) {
-        container.className = 'footer';
-        container.innerHTML = `<p>&copy; ${new Date().getFullYear()} Kalp Pariya. All Rights Reserved.</p>`;
-    }
-}
--- js/common/settings.js
+++
@@ -1,4 +0,0 @@
-export function initSettingsModal() {
-    // This function will be properly implemented in a future step.
-    // For now, it does nothing to avoid errors.
-}
