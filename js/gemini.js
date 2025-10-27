ceb48c5b9e153c0db8972ff4e46c28d32232098f
k4lp <61631873+k4lp@users.noreply.github.com>
2025-10-27 05:54:46 +0530

    Update gemini.js

--- js/gemini.js
+++ js/gemini.js
@@ -133,19 +133,19 @@
         safetySettings: options.safetySettings || [
           {
             category: "HARM_CATEGORY_HARASSMENT",
-            threshold: "BLOCK_MEDIUM_AND_ABOVE"
+            threshold: "OFF"
           },
           {
             category: "HARM_CATEGORY_HATE_SPEECH", 
-            threshold: "BLOCK_MEDIUM_AND_ABOVE"
+            threshold: "OFF"
           },
           {
             category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            threshold: "BLOCK_MEDIUM_AND_ABOVE"
+            threshold: "OFF"
           },
           {
             category: "HARM_CATEGORY_DANGEROUS_CONTENT",
-            threshold: "BLOCK_MEDIUM_AND_ABOVE"
+            threshold: "OFF"
           }
         ]
       };
